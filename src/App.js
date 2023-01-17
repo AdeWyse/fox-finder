d203a475a82b88265996921fa940a2a60ebe4c2a
AdeWYyse <adelinewyse@gmail.com>
2023-01-17 15:46:33 -0300

    Project to get a fox photo from a api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+  const [photo, setPhoto] = useState('');
+
+  const find = () => {
+      axios.get("https://randomfox.ca/floof/").then((response) => {
+        setPhoto(response.data.image);
+        console.log(response.data.link);
+    });
+  }
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 className='siteTitle'>FoxFinder</h3>
       </header>
+      <main>
+        <button className='finder' onClick={find}>Find a Fox!</button>
+        <div className='imgContainer'>
+        {photo !== '' ?  (<img className='imgFound'  src={photo} alt='Random fox.'></img>) : (<div></div>)}
+            
+        </div>
+      </main>
+      <footer className="App-footer">
+        <div className='pads'>FoxFinder by  <a href='https://github.com/AdeWyse' target ='blank'>AdeWyse</a></div>
+        <div className='pads'> Photos from <a href='https://randomfox.ca/' target ='blank'>https://randomfox.ca/</a></div>
+      </footer>
     </div>
   );
 }
